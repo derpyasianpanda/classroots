5685dabec92d422b467b9efcfce3c4bf2d00cc84
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-08-15 20:40:16 -0700

    Fix KV's stupid mistake

--- server/app.js
+++ server/app.js
@@ -26,4 +26,4 @@ app.get("*", (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, console.log(`Server running on port ${PORT} ${res}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
