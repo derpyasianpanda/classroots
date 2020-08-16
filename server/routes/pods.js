3cc9f30bc257fbfcf53300c7ad2943d29658c137
Judah Wyllie <judahw@uw.edu>
2020-08-16 08:45:35 -0700

    more endpoints

--- server/routes/pods.js
+++ server/routes/pods.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/firebase");
 
 router.get("/", async (req, res) => {
     res.send("This is a pods route");
@@ -19,7 +20,59 @@ router.post("/create_pod", async (req, res) => {
             location: location,
             tags: tags ? tags : []
         }
-        const res = await db.collection('pod').add(data);
+        const pod = await db.collection('pod').add(data);
+        res.json({
+            status: "Successfully created Pod",
+            id: pod.id
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "Server could not process the request"});
+    }
+});
+
+router.post("/search_pods", async (req, res) => {
+    let { grade, location, tags } = req.query;
+    if (!grade || !location) {
+        return res.status(400).json({ status: "Missing query parameters" })
+    }
+    try {
+        const pods = await db.collection('pod')
+            .where('grade', '==', grade)
+            .where('location', 'in', [location, null])
+            .where('tags', 'array-contains-any', tags)
+            .get();
+        if (pods.empty) {
+            res.json({
+                status: "No Pods found",
+                pods: []
+            });
+        }
+        else {
+            res.json({
+                status: "Pods found",
+                pods: pods
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "Server could not process the request"});
+    }
+});
+
+router.post("/add_user", async (req, res) => {
+    let { pod_id, user_id } = req.query;
+    if (!grade || !location) {
+        return res.status(400).json({ status: "Missing query parameters" })
+    }
+    try {
+        const userRef = db.collection('user').doc(user_id);
+        await userRef.update({
+          pods: admin.firestore.FieldValue.arrayUnion(pod_id)
+        });
+        res.json({
+            status: "Added User to Pod"
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ status: "Server could not process the request"});
