9f50b37c7ba4fccbc119c91f624368e86b31302e
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-08-15 18:25:19 -0700

    Add client hosting to server

---
+++ server/app.js
@@ -0,0 +1,27 @@
+// Dependencies
+const dotenv = require("dotenv");
+const express = require("express");
+
+const PORT = process.env.PORT || 8000;
+
+// Configuration
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+
+// Request Body Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static("../client/build"));
+
+// Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/share", require("./routes/pods"));
+
+// Redirects all other requests to main page
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+// Start Server
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
