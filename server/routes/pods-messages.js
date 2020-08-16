5bf9ba031f1c2bba50b22949372220104f9ce499
Judah Wyllie <judahw@uw.edu>
2020-08-16 09:33:07 -0700

    imports

--- server/routes/pods-messages.js
+++ server/routes/pods-messages.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../config/firebase");
 
 router.get("/", async (req, res) => {
     res.send("This is a pods/messages route");
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const router = express.Router();
+const db = require("../config/firebase");
 
 router.get("/", async (req, res) => {
     try {
