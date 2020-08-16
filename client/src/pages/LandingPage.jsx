3d1ff56fbd78ea46ff70f4f71f82c2948c7b2342
Joshua Maza <joshuasadirimaza@gmail.com>
2020-08-15 18:40:44 -0700

    basic page nav

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './LandingPage.css';
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>landing page</h1>
+                    <p>wow</p>
+
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
