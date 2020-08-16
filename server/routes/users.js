3cf16c71d22e3a9291954348430e3d2e8c0df5f5
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-08-16 01:36:55 -0700

    KV is actually retarded

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
     res.send("This is a users route");
+});
+
+router.get("/login", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get("/login/callback",
+    passport.authenticate("google", {
+        successRedirect: "/",
+        failureRedirect: "/"
+    })
+);
+
+router.get("/logout", (req, res) => {
+    try {
+        req.logout();
+        req.session.destroy();
+        res.json({ status: "Successfully" });
+    } catch (error) {
+        res.status(500).json({ status: "Server error in logging out" });
+    }
 });
 
 module.exports = router;
