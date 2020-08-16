f13651cb0b1292c5567e2b43fcf71e3eb569894b
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-08-15 23:07:01 -0700

    Add authentication skeleton

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -3,7 +3,8 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./classroots-hack20-firebase-adminsdk-df788-c34517a810.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://classroots-hack20.firebaseio.com"
 });
 
 const db = admin.firestore();
---
+++ server/config/passport.js
@@ -0,0 +1,32 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const db = require("./config/firebase.js");
+
+// Configures a given passport to use the Google Strategy
+module.exports = passport => {
+    passport.use(
+        new GoogleStrategy({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/users/login/callback"
+        },
+        (accessToken, refreshToken, profile, callback) => {
+            // Implement User creation here
+            // profile contains all the information
+            const newUser = {
+                googleID: profile.id,
+                displayName: profile.displayName,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                image: profile.photos[0].value
+            };
+            return callback(null, null);
+        }
+    ));
+
+    passport.serializeUser((user, done) => done(null, user.id));
+
+    passport.deserializeUser((id, callback) => {
+        // TODO: Find how to query Firebase
+        return callback(null, null);
+    });
+};
