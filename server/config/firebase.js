e7caaefd85ecb49b5fd10af622bdca2249cc403e
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-08-16 11:37:22 -0700

    Configure firebase for heroku deployment

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./classroots-hack20-firebase-adminsdk-df788-c34517a810.json");
+const serviceAccount = process.env.FIREBASE || require("./classroots-hack20-firebase-adminsdk-df788-c34517a810.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
