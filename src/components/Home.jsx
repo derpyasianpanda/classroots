1daf95c8cdd72ad3359ba75b77372a43da5e0aa8
KV Le <32186663+derpyasianpanda@users.noreply.github.com>
2020-08-30 10:13:34 -0700

    ✨ Beautify Landing page and 🖼 Add resources folder

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,26 +21,59 @@ const Home = () => {
 
     return (
         <main>
-            {user ?
-            <>
-                <h1>Hello {user.displayName}</h1>
-                <button onClick={() => fireauth.signOut()}>Sign Out</button>
-            </>
-            :
-            <button
-                onClick={() => {
-                    fireauth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-                }}
-            >
-                Sign In
-            </button>}
+            <section>
+                <img src="resources/classroots-logo.gif" alt="Classroots Logo"/>
+                <h1>A grassroots approach to home learning</h1>
+            </section>
+            <section className="info">
+                <img style={{ width: "400px" }} src="resources/smallplant.png" alt="Small Plant"/>
+                <div>
+                    <h1>
+                        Grassroots + Learning
+                    </h1>
+                    <p>
+                        <em>Classroots</em> allows parents of homeschooled or hybrid schooled
+                        students to connect and discuss cirriculum and education opportunities
+                        for their children. These connections can be based of of various things
+                        like location, interest, or subject.
+                    </p>
+                </div>
+            </section>
+            <section className="info">
+                <img style={{ width: "400px" }} src="resources/pod.png" alt="Pea Pod"/>
+                <div>
+                    <h1>What are pods?</h1>
+                    <p>
+                        <em>Pods</em> are how Classroots brings people together. Pods are groups of 5-50
+                        parents who are able to connect and share educational resources for their
+                        children. Each Pod is dedicated and tailored to specific children needs,
+                        and offers a convenient way for parents to connect through group
+                        messaging and sharing. New Pods are always forming and evolving so that
+                        parents are readily able to find the right communities.
+                    </p>
+                </div>
+            </section>
 
-            <br/>
-            <Link to="/pods">All Pods</Link>
-            <br/>
-            <Link to="/users">All Users</Link>
+            <section>
+                {user ?
+                <>
+                    <h1>Hello {user.displayName}</h1>
+                    <button onClick={() => fireauth.signOut()}>Sign Out</button>
+                </>
+                :
+                <button
+                    onClick={() => {
+                        fireauth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+                    }}
+                >
+                    Sign In
+                </button>}
+            </section>
 
-            <h3>Current Pods</h3>
+            {/* <Link to="/pods">All Pods</Link>
+            <Link to="/users">All Users</Link> */}
+
+            {/* <h3>Current Pods</h3>
             <ul>
                 {podsInfo && podsInfo.map(podInfo => {
                     return (
@@ -52,7 +85,7 @@ const Home = () => {
                         </li>
                     );
                 })}
-            </ul>
+            </ul> */}
         </main>
     );
 };
